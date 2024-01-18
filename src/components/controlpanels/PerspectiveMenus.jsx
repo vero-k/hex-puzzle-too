c056d7d6ff3f689c971a3775d492b9d03ff3a592
Vero <145688375+vero-k@users.noreply.github.com>
2024-01-18 17:09:13 +0100

    dependency thing

--- src/components/controlpanels/PerspectiveMenus.jsx
+++ src/components/controlpanels/PerspectiveMenus.jsx
@@ -7,6 +7,10 @@ export const PerspectiveMenus = (props) => {
 
     const perspective = useContext(PerspectiveContext)
 
+    const togglePerspective = () =>{
+
+    }
+    
     return(<>
     
         <Button variant="primary" onClick={() => togglePerspective()}>
